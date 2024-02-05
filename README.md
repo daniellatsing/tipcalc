4022d6bc79d85974079e664a798ae69925c4da36
Daniella <daniellatsing@gmail.com>
2024-02-05 02:51:51 -0800

    complete assignment with extra credit

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 # Tip Calculator
-...because math is hard. I want an app that knows how to calculate the tip on a dinner at a nice restaurant.
+...because math is hard. I want an app that knows how to calculate the tip on a dinner at a nice 
+restaurant.
 
 ## Stories 
-- As a user, when I launch the application, I should see a text edit (labeled "Amount") allowing me to enter the service charge, and a pushbutton labeled "Tip". The "Tip" pushbutton should be disabled.
-- As a user, when I enter data into the service charge textedit, only numbers should be allowed.
-- As a user, when I enter data into the service charge textedit, it should read like a USD currency amount ("$15.23"); two decimal places, and prefixed by "$".
-- As a user, when I enter data into the service charge textedit, it should enable the "Tip" pushbutton.
-- As a user, when I push the "Tip" pushbutton, it should calculate the tip (15% of the value in the service charge textedit) and display that value in a Toast message. The value displayed should be displayed as USD currency ("$15.23"): two decimal places, prefixed by "$".
+- As a user, when I launch the application, I should see a text edit (labeled "Amount") allowing 
+me to enter the service charge, and a push button labeled "Tip". The "Tip" push button should be 
+disabled.
+- As a user, when I enter data into the service charge text edit, only numbers should be allowed.
+- As a user, when I enter data into the service charge text edit, it should read like a USD currency 
+amount ("$15.23"); two decimal places, and prefixed by "$".
+- As a user, when I enter data into the service charge text edit, it should enable the "Tip" push 
+button.
+- As a user, when I push the "Tip" push button, it should calculate the tip (15% of the value in the 
+service charge text edit) and display that value in a Toast message. The value displayed should be 
+displayed as USD currency ("$15.23"): two decimal places, prefixed by "$".
 
 ## Extra Credit 
-- [ ] Add a spinner or some other control to choose the tip amount: 10%, 15%, 18% or 20%, and calculate accordingly 
-- [ ] Make sure that only two decimal points ever appear, and that the math is always correct 
+- [x] Add a spinner or some other control to choose the tip amount: 10%, 15%, 18% or 20%, and 
+calculate accordingly 
+- [x] Make sure that only two decimal points ever appear, and that the math is always correct 
--- app/src/main/java/edu/uw/ischool/dtsing/tipcalculator/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/dtsing/tipcalculator/MainActivity.kt
@@ -2,10 +2,97 @@ package edu.uw.ischool.dtsing.tipcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import java.text.NumberFormat
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    // Set up variables for later initialization
+    private lateinit var tipButton: Button
+    private lateinit var serviceCharge: EditText
+    private lateinit var tipSpinner: Spinner
+    private var tipPercent = 0.15
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize views
+        serviceCharge = findViewById(R.id.serviceCharge)
+        tipButton = findViewById(R.id.tipButton)
+        tipButton.isEnabled = false // Disable button upon launching application
+        tipSpinner = findViewById(R.id.tipSpinner)
+
+        tipSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parentView: AdapterView<*>, selectedItemView: View?, position: Int, id: Long) {
+                updateTipPercentage(position)
+            }
+
+            override fun onNothingSelected(parentView: AdapterView<*>) {}
+        }
+
+        // Text input listener
+        serviceCharge.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                formatCurrency(this, s)
+            }
+        })
+
+        // Calculate tip and display in a Toast message
+        tipButton.setOnClickListener {
+            val amountString = serviceCharge.text.toString().replace("$", "")
+            val amount = amountString.toDouble()
+            val tip = amount * tipPercent
+            val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+
+            Toast.makeText(this, formattedTip, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateTipPercentage(position: Int) {
+        when (position) {
+            0 -> tipPercent = 0.10
+            1 -> tipPercent = 0.15
+            2 -> tipPercent = 0.18
+            3 -> tipPercent = 0.20
+        }
+    }
+
+    private fun formatCurrency(watcher: TextWatcher, s: Editable?) {
+        val originalSelection = serviceCharge.selectionStart
+        val amountString = s.toString().replace("$", "")
+        val amount = amountString.toDoubleOrNull() ?: 0.0
+
+        tipButton.isEnabled = amount >= 0.0
+
+        serviceCharge.removeTextChangedListener(watcher)
+
+        val formatted =
+            if (amount == 0.0) {
+                "$0.00"
+            } else {
+                // Remove any "$", ",", or "."
+                val cleanString = amountString.replace("""[$,.]""".toRegex(), "")
+                val parsed = cleanString.toDouble()
+                NumberFormat.getCurrencyInstance().format(parsed / 100)
+            }
+
+        serviceCharge.setText(formatted)
+
+        // Adjust cursor location
+        val newSelection = originalSelection + formatted.length - s.toString().length
+        serviceCharge.setSelection(newSelection)
+
+        serviceCharge.addTextChangedListener(watcher)
     }
 }
